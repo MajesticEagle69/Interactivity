4c667d17c9610847602fbead553efa6e39a9da78
LasseBachmann <lasse.m.bachmann@gmail.com>
2025-09-04 14:32:43 +0200

    Updated code structure

--- pressure/script.js
+++ pressure/script.js
@@ -1,68 +1,107 @@
+const settings = Object.freeze({
+    deflateFactor: 0.8,
+    inflateFactor: 0.0095
+});
+
 /**
  * @typedef {{
- * press: boolean
 *  deflating: boolean
 *  infalteValue: number
 *  defaltevalue: number
-*  pressure:number
+*  pressure: number
  * }} State
  */
 
 
-let deflating = false;
-
-let infalteValue = 3;
-let defaltevalue = 7;
-let pressure = 50;
+/** @type State */
+let state = Object.freeze({
+  deflating: false,
+  infalteValue: 3,
+  defaltevalue: 7,
+  pressure: 50
+});
 
 let div;
 
 document.addEventListener("DOMContentLoaded", setup);
 
 function setup() {
-    div = document.getElementById('target');
+    div = /** @type HTMLElement|null */ (document.getElementById('target'));
 
-    loop();
+    document.addEventListener(`keydown`, onKeyDown);
+    document.addEventListener(`keyup`, onKeyUp);
+
+    setInterval(use, 5);
 }
 
-function loop() {
-    document.addEventListener("keydown", function(event) {        
-        if(!deflating) {
-            pressure += infalteValue;
-            infalteValue -= 0.008;
+/**
+ * @param {State} pressure
+ * @returns
+ */
+function use(){
+    const {pressure} = state;
 
-            if (infalteValue < 0.001) {
-                infalteValue = 0.001;
-            }
+    div.style.height = `${pressure}px`;
+    div.style.width = `${pressure}px`;
+}
 
-            div.style.height = `${pressure}px`;
-            div.style.width = `${pressure}px`;
+/**
+ * @param {state} pressure
+ * @param {State} infalteValue
+ * @param {State} defaltevalue
+ * @param {State} deflating
+ * @returns
+ */
+function onKeyDown() {
+    let {deflating, pressure, infalteValue, defaltevalue} = state;
+    const {inflateFactor, deflateFactor} = settings;
+    if(!deflating) {
+        pressure += infalteValue;
+        infalteValue -= inflateFactor;
 
-            if (pressure > 700) {
-            pressure = 700;
-            }
-            console.log(pressure);
-        }else {
-            pressure -= defaltevalue;
-            if (pressure < 50) {
-                pressure = 50;
-            }
-            defaltevalue += 0.8;
-
-            div.style.height = `${pressure}px`;
-            div.style.width = `${pressure}px`;
-
-            console.log(pressure);
+        if (infalteValue < 0.001) {
+            infalteValue = 0.001;
         }
 
-    });
+        if (pressure > 700) {
+        pressure = 700;
+        }
+        console.log(pressure);
+    }else {
+        pressure -= defaltevalue;
+        if (pressure < 50) {
+            pressure = 50;
+        }
+        defaltevalue += deflateFactor;
 
-    document.addEventListener("keyup", function(event) {
-        deflating = !deflating;
+        console.log(pressure);
+    }
 
-        infalteValue = 3;
-        defaltevalue = 7;
+    saveState({ pressure, infalteValue, defaltevalue, deflating });
+}
 
-        console.log(deflating);
+/**
+ * @param {State} pressure
+ * @param {State} infalteValue
+ * @param {State} defaltevalue
+ * @param {State} deflating
+ * @returns
+ */
+function onKeyUp() {
+    let {deflating, infalteValue, defaltevalue, pressure} = state;
+    deflating = !deflating;
+
+    infalteValue = 3;
+    defaltevalue = 7;
+
+    console.log(deflating);
+
+    saveState({ pressure, infalteValue, defaltevalue, deflating });
+}
+
+function saveState(s) {
+    state = Object.freeze({
+        ...state,
+        ...s
     });
 }
